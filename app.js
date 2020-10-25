9eeda91d4ef23948c9f53b441d131ce4ce0bd30d
baxter-bouncing <35816026+baxter-bouncing@users.noreply.github.com>
2020-10-25 14:52:39 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function testGS() {
 
-const url = "https://script.googleusercontent.com/macros/echo?user_content_key=hSHH71XeoP9X18H2CUvD3EbEId4nvTuwkMJZfou4WM4ERwsB6-eJjzWlWHcNo_MIkeF5Fvj0KXIspKoV7tjpj9Wc_-bInMJ6m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDvvlVnGxmPkhR4g7m7MXiHLbmaH4KsICyjNkBQBEMFkLxJ1AclEdOhmz65PknAiSKV-rV9yhQ38&lib=MvPG1sc-IdFrZFOKWcXF8bPLSJHYMIpDM";
+const url = "https://script.googleusercontent.com/macros/echo?user_content_key=BnfkN5oSrKlffUiIDcGNfBEwEXRymMLlhoKcaNd8hwzUA8BcYoTdUVqX5_jtKBGdm_Fqg63tpbyK4U-RbJb2a2VKb17UpS7um5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOQ3a7VIZY1YB-_aSAHhWLcgT49K6KgVN_U7LiEG-UL7GiqzjesouW24tkgnH9X6eQ&lib=MN5mtiBr4FJ__o3jOdxIUl9tiVCmZWsC4";
+      //"https://script.googleusercontent.com/macros/echo?user_content_key=hSHH71XeoP9X18H2CUvD3EbEId4nvTuwkMJZfou4WM4ERwsB6-eJjzWlWHcNo_MIkeF5Fvj0KXIspKoV7tjpj9Wc_-bInMJ6m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDvvlVnGxmPkhR4g7m7MXiHLbmaH4KsICyjNkBQBEMFkLxJ1AclEdOhmz65PknAiSKV-rV9yhQ38&lib=MvPG1sc-IdFrZFOKWcXF8bPLSJHYMIpDM";
 
 fetch(url)
     .then(d => d.json())
@@ -17,7 +18,8 @@ fetch(url)
 
 function addGS() {
 
-    const url = "https://script.google.com/macros/s/AKfycbzz6tCnBGakoqexU1N0COXgRA3-s83BydQs6X5qw8S4mywBx6w/exec";
+    const url = "https://script.google.com/macros/s/AKfycby62jOdGnnfx-GhqPEPFDsEGPhg165LOoAsiX8ObHzPd07Jwrho/exec";
+          //"https://script.google.com/macros/s/AKfycbzz6tCnBGakoqexU1N0COXgRA3-s83BydQs6X5qw8S4mywBx6w/exec";
     fetch(url, {
         method: 'POST',
         mode: 'no-cors',
@@ -26,7 +28,7 @@ function addGS() {
             'Content-Type': 'application/json'
         },
         redirect: 'follow',
-        body: JSON.stringify({name: "Olivia"})
+        body: JSON.stringify({"Subcontractor":"Rusty Pipe Plumbing","Job_Name":"BPlaza","Description_of_Work":"Patch Roof","Date_of_Work":"10/29/2020","Amount":"500.00"})
     });
  
     }
