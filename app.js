36537bf09bc42761b87fbb39d9e455c99bb56fd5
baxter-bouncing <35816026+baxter-bouncing@users.noreply.github.com>
2020-10-25 15:07:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function testGS() {
-
-const url = "https://script.google.com/macros/s/AKfycby62jOdGnnfx-GhqPEPFDsEGPhg165LOoAsiX8ObHzPd07Jwrho/exec"
+var url = "https://script.google.com/macros/s/AKfycby62jOdGnnfx-GhqPEPFDsEGPhg165LOoAsiX8ObHzPd07Jwrho/exec";
       //"https://script.googleusercontent.com/macros/echo?user_content_key=BnfkN5oSrKlffUiIDcGNfBEwEXRymMLlhoKcaNd8hwzUA8BcYoTdUVqX5_jtKBGdm_Fqg63tpbyK4U-RbJb2a2VKb17UpS7um5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnOQ3a7VIZY1YB-_aSAHhWLcgT49K6KgVN_U7LiEG-UL7GiqzjesouW24tkgnH9X6eQ&lib=MN5mtiBr4FJ__o3jOdxIUl9tiVCmZWsC4";
       //"https://script.googleusercontent.com/macros/echo?user_content_key=hSHH71XeoP9X18H2CUvD3EbEId4nvTuwkMJZfou4WM4ERwsB6-eJjzWlWHcNo_MIkeF5Fvj0KXIspKoV7tjpj9Wc_-bInMJ6m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDvvlVnGxmPkhR4g7m7MXiHLbmaH4KsICyjNkBQBEMFkLxJ1AclEdOhmz65PknAiSKV-rV9yhQ38&lib=MvPG1sc-IdFrZFOKWcXF8bPLSJHYMIpDM";
 
@@ -8,7 +7,7 @@ fetch(url)
     .then(d => d.json())
     .then(d => {
 
-       document.getElementById("app").textContent = d[0].status;
+       document.getElementById("app").textContent = d;
 
 
     });
@@ -19,7 +18,7 @@ fetch(url)
 
 function addGS() {
 
-    const url = "https://script.google.com/macros/s/AKfycby62jOdGnnfx-GhqPEPFDsEGPhg165LOoAsiX8ObHzPd07Jwrho/exec";
+    var url = "https://script.google.com/macros/s/AKfycby62jOdGnnfx-GhqPEPFDsEGPhg165LOoAsiX8ObHzPd07Jwrho/exec";
           //"https://script.google.com/macros/s/AKfycbzz6tCnBGakoqexU1N0COXgRA3-s83BydQs6X5qw8S4mywBx6w/exec";
     fetch(url, {
         method: 'POST',
